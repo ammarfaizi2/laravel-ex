91fb315d3f097b8029c3f36da70eb1f2b72df9c6
Ammar Faizi <ammarfaizi2@gmail.com>
2018-01-15 10:56:22 +0700

    Fix files

--- app/Http/Controllers/ClefController.php
+++ app/Http/Controllers/ClefController.php
@@ -3,34 +3,35 @@
 
 namespace App\Http\Controllers;
 
+use DB;
 use Config;
-use App\Models\Authentication;
-use App\Models\Balance;
-use App\Models\CoinVote;
-use App\Models\Deposit;
-use App\Models\FeeTrade;
-use App\Models\FeeWithdraw;
-use App\Models\Giveawayclaims;
-use App\Models\Giveaways;
-use App\Models\Limits;
-use App\Models\Market;
 use App\Models\News;
-use App\Models\Notifications;
-use App\Models\Order;
 use App\Models\Post;
 use App\Models\Role;
-use App\Models\SecurityQuestion;
-use App\Models\Setting;
-use App\Models\Trade;
-use App\Models\Trade;
-use App\Models\Transfer;
 use App\Models\User;
+use App\Models\Vote;
+use App\Models\Order;
+use App\Models\Trade;
+use App\Models\Limits;
+use App\Models\Market;
+use App\Models\Wallet;
+use App\Models\Balance;
+use App\Models\Deposit;
+use App\Models\Setting;
+use App\Models\FeeTrade;
+use App\Models\CoinVote;
+use App\Models\Transfer;
+use App\Models\Withdraw;
+use App\Models\Giveaways;
+use App\Models\FeeWithdraw;
+use App\Models\Notifications;
+use App\Models\Giveawayclaims;
+use App\Models\Authentication;
+use App\Models\WalletLimitTrade;
+use App\Models\SecurityQuestion;
 use App\Models\UserAddressDeposit;
 use App\Models\UserSecurityQuestion;
-use App\Models\Vote;
-use App\Models\Wallet;
-use App\Models\WalletLimitTrade;
-use App\Models\Withdraw;
+use Illuminate\Support\Facades\Session;
 
 class ClefController extends Controller
 {
