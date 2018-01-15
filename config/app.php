773016a2e184fd3a09c38940093acc1c1ab78507
Ammar Faizi <ammarfaizi2@gmail.com>
2018-01-15 09:07:00 +0700

    Add aliases

--- config/app.php
+++ config/app.php
@@ -226,6 +226,9 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        // nan
+        'Form' => Illuminate\Html\FormFacade::class,
+        'Html' => Illuminate\Html\HtmlFacade::class,
     ],
 
 ];
