17afce25d79fc3eb3002a12d4586c795879bf09d
Ammar Faizi <ammarfaizi2@gmail.com>
2018-01-17 21:09:01 +0700

    Fix filename and class name (case sensitive reason)

--- app/JsonRPCClient.php
+++ app/JsonRPCClient.php
@@ -30,7 +30,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
  * @author sergio <jsonrpcphp@inservibile.org>
  */
-class jsonRPCClient {
+class JsonRPCClient {
         /**
          * Debug state
          *
