4b2a6bac3dc1a6cb085046c24b352069c7d36b6a
Ammar Faizi <ammarfaizi2@gmail.com>
2018-01-19 11:00:09 +0700

    Fix ajax route and url

--- resources/views/blocks/buyform.blade.php
+++ resources/views/blocks/buyform.blade.php
@@ -218,9 +218,9 @@ function doPostTradeOrder(tradeArray){
 		var ajax_trade_url;
 		
 		if(type == 'buy'){
-			ajax_trade_url = '<?php echo route('OrderController@doBuy')?>';
+			ajax_trade_url = '<?php echo route('order.do.buy')?>';
 		}else if(type == 'sell'){
-			ajax_trade_url = '<?php echo route('OrderController@doSell')?>';
+			ajax_trade_url = '<?php echo route('order.do.sell')?>';
 		}
 		
 	$.ajax({
