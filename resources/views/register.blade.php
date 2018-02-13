156208de6c87a01edd7c36bcc99e0d3d2206b014
Ammar Faizi <ammarfaizi2@gmail.com>
2018-02-13 14:41:46 +0700

    Create min username length on env file

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -239,7 +239,7 @@ https://developers.google.com/recaptcha/docs/verify
                 },
                 username: {
                     required: true,
-                    minlength: 5,
+                    minlength: {{ env("MIN_USERNAME_LENGTH", 5) }},
 					CharNumsOnly: true,
                 }
             },
