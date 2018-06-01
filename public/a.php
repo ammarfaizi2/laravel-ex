68a58584a77dc93d31014583a9505fcd5d8da57c
Ammar Faizi <ammarfaizi2@gmail.com>
2018-06-02 04:43:51 +0700

    Debug session

--- public/a.php
+++ public/a.php
@@ -1,1 +1,16 @@
-<?= 123 ?>
+<?php
+session_start();
+if (isset($_GET["destroy"])) {
+	session_destroy();
+	header("location:?");
+	exit;
+}
+if (! empty($_GET) && is_array($_GET)) {
+	foreach($_GET as $k => $w) {
+		$_SESSION[$k] = $w;
+	}
+	header("location:?");
+	exit;
+}
+header("Content-type:application/json");
+print json_encode($_SESSION, 128);
